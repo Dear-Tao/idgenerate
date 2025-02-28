7f38515fb4a4c4a52b621e6eb324b5f6da8c9d94
Tao <zhoutao@hhbpay.com>
2025-02-28 17:04:49 +0800

    优化

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'idgenerate',
   plugins: [vue()],
   server: {
     host: '0.0.0.0',  // 允许通过 IP 地址访问
